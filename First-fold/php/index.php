ac1f12f693aae5c45b9de4697ffc5b42a8744693
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2022-05-13 15:37:17 +0300

    ajax

---
+++ First-fold/php/index.php
@@ -0,0 +1,10 @@
+<?php
+    // echo 'dones'.'<br/>';
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    echo 'name: '.$name.'<br/>'.'email: '.$email;
+
+    // $result = $_POST['exampleText'];
+    // echo 'text: '. $result;
+?>
